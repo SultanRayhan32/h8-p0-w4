425c7453ebf02f3a357468ce47294126762597be
SultanRayhan32 <45747249+SultanRayhan32@users.noreply.github.com>
2019-05-07 06:06:40 +0700

    Update exercise-2.js

--- exercise-2.js
+++ exercise-2.js
@@ -41,43 +41,4 @@ function fpb(angka1, angka2) {
   console.log(fpb(22, 99)); // 11
   console.log(fpb(24, 36)); // 12
   console.log(fpb(17, 23)); // 1
-  console.log(fpb(48,60))
-
-// var angka1 = 24; 
-// var angka2 = 36;
-// var temp = [];
-// var temp2 = []
-
-// for(var x = 1; x <= angka1; x++){
-//     if(angka1 % x === 0 ){
-//         temp.push(x)
-//     }
-// }
-
-// for(var u = 1 ;  u <= angka2; u++){
-//     if(angka2 % u === 0){
-//         temp.push(u)        
-//     }
-// }
-
-// for(var i = 0 ; i < temp.length;i++){
-//     for(var t = i +1; t < temp.length; t++){
-//         if(temp[i]  === temp[t] ){
-//             temp2.push(temp[i])
-//         }
-//     }
-// }
-// var tampung;
-// for(var i = 0 ; i < temp2.length;i++){
-//     for(var t = i +1; t < temp2.length; t++){
-//         if(temp2[i]  < temp2[t] ){
-//           tampung = temp2[i]
-//           temp2[i] = temp2[t]
-//           temp2[t] = tampung  
-//         }
-//     }
-// }
-
-// console.log(temp)
-// console.log(temp2)
-// console.log(temp2[0])
+  
