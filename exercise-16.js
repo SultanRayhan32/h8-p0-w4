276c8e353cc1582ed87e93e836d7a8a0b77db929
SultanRayhan32 <45747249+SultanRayhan32@users.noreply.github.com>
2019-05-08 13:59:02 +0700

    Create exercise-16.js

---
+++ exercise-16.js
@@ -0,0 +1,94 @@
+function graduates (students) {
+    // Code disini
+    var hasil = {};
+    for(var x = 0; x < students.length; x++){
+        if(students[x].score > 75){
+            if(hasil[students[x].class] === undefined){
+                hasil[students[x].class] = []
+            }
+            hasil[students[x].class].push({
+                name : students[x].name ,
+                score : students[x].score
+            })
+        }
+    }
+    return hasil
+
+  }
+  
+  console.log(graduates([
+    {
+      name: 'Dimitri',
+      score: 90,
+      class: 'foxes'
+    },
+    {
+      name: 'Alexei',
+      score: 85,
+      class: 'wolves'
+    },
+    {
+      name: 'Sergei',
+      score: 74,
+      class: 'foxes'
+    },
+    {
+      name: 'Anastasia',
+      score: 78,
+      class: 'wolves'
+    }
+  ]));
+  
+  // {
+  //   foxes: [
+  //     { name: 'Dimitri', score: 90 }
+  //   ],
+  //   wolves: [
+  //     { name: 'Alexei' , score: 85 },
+  //     { name: 'Anastasia', score: 78 }
+  //   ]
+  // }
+  
+  console.log(graduates([
+    {
+      name: 'Alexander',
+      score: 100,
+      class: 'foxes'
+    },
+    {
+      name: 'Alisa',
+      score: 76,
+      class: 'wolves'
+    },
+    {
+      name: 'Vladimir',
+      score: 92,
+      class: 'foxes'
+    },
+    {
+      name: 'Albert',
+      score: 71,
+      class: 'wolves'
+    },
+    {
+      name: 'Viktor',
+      score: 80,
+      class: 'tigers'
+    }
+  ]));
+  
+  // {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
